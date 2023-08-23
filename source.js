230b3b28d488ce7aae20cd1db13308c5d5817297
AlanOixDeCoco <49500226+AlanOixDeCoco@users.noreply.github.com>
2023-08-23 22:34:25 +0200

    update projects page

--- source.js
+++ source.js
@@ -1,3 +1,57 @@
+// Constants
+const projects = [
+    {
+        name: `18 Seconds`,
+        description: ``,
+        screenshots: [
+            `url("Assets/Images/Games/18seconds_00.png")`,
+            //...
+        ]
+        //...
+    },
+    {
+        name: `Escape It!`,
+        description: ``,
+        screenshots: [
+            `url("Assets/Images/Games/18seconds_00.png")`,
+            //...
+        ]
+    },
+    {
+        name: `Escape I!`,
+        description: ``,
+        screenshots: [
+            `url("Assets/Images/Games/18seconds_00.png")`,
+            //...
+        ]
+    },
+    {
+        name: `Escape t!`,
+        description: ``,
+        screenshots: [
+            `url("Assets/Images/Games/18seconds_00.png")`,
+            //...
+        ]
+    },
+];
+
+// Event functions
 function goToDiv(div){
     document.getElementById(div).scrollIntoView();
 }
+
+// Functions
+function createProjectButton(project){
+
+}
+
+// Program
+projects.forEach(project => {
+    document.getElementById("projects_list").innerHTML += `
+        <div class="btn_project" id="btn_${project.name}">
+            <div class="btn_project_name">${project.name}</div>
+        </div>
+    `;
+
+    document.getElementById(`btn_${project.name}`).style.backgroundImage = project.screenshots[0];
+});
