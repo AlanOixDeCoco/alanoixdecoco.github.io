4f592cca6093f2d3a2b1ae81b5f0499f39df66d2
AlanOixDeCoco <49500226+AlanOixDeCoco@users.noreply.github.com>
2023-08-24 22:48:55 +0200

    Finished projects scroller

--- source.js
+++ source.js
@@ -2,56 +2,146 @@
 const projects = [
     {
         name: `18 Seconds`,
+        year: 2021,
         description: ``,
+        icon: `url("Assets/Images/Games/18seconds/icon.png")`,
         screenshots: [
-            `url("Assets/Images/Games/18seconds_00.png")`,
+            `url("Assets/Images/Games/18seconds/screenshot_00.png")`,
             //...
         ]
         //...
     },
     {
         name: `Escape It!`,
+        year: 2021,
         description: ``,
+        icon: `url("Assets/Images/Games/EscapeIt/icon.png")`,
         screenshots: [
-            `url("Assets/Images/Games/18seconds_00.png")`,
             //...
         ]
     },
     {
-        name: `Escape I!`,
+        name: `Workbook`,
+        year: 2022,
         description: ``,
+        icon: `url("Assets/Images/Games/Workbook/icon.png")`,
         screenshots: [
-            `url("Assets/Images/Games/18seconds_00.png")`,
             //...
         ]
     },
     {
-        name: `Escape t!`,
+        name: `The Glorious Pasteur`,
+        year: 2022,
         description: ``,
+        icon: `url("Assets/Images/Games/TheGloriousPasteur/icon.png")`,
         screenshots: [
-            `url("Assets/Images/Games/18seconds_00.png")`,
+            //...
+        ]
+    },
+    {
+        name: `Windshield Warfare`,
+        year: 2022,
+        description: ``,
+        icon: `url("Assets/Images/Games/WindshieldWarfare/icon.png")`,
+        screenshots: [
+            //...
+        ]
+    },
+    {
+        name: `RPG Battle`,
+        year: 2022,
+        description: ``,
+        icon: `url("Assets/Images/Games/RpgBattle/icon.png")`,
+        screenshots: [
+            //...
+        ]
+    },
+    {
+        name: `Sidescroller`,
+        year: 2023,
+        description: ``,
+        icon: `url("Assets/Images/Games/Sidescroller/icon.png")`,
+        screenshots: [
+            //...
+        ]
+    },
+    {
+        name: `(Un)breakable`,
+        year: 2023,
+        description: ``,
+        icon: `url("Assets/Images/Games/UnBreakable/icon.png")`,
+        screenshots: [
+            //...
+        ]
+    },
+    {
+        name: `BOB`,
+        year: 2023,
+        description: ``,
+        icon: `url("Assets/Images/Games/Bob/icon.png")`,
+        screenshots: [
+            //...
+        ]
+    },
+    {
+        name: `Hero Defense`,
+        year: 2023,
+        description: ``,
+        icon: `url("Assets/Images/Games/HeroDefense/icon.png")`,
+        screenshots: [
+            //...
+        ]
+    },
+    {
+        name: `Night Knight`,
+        year: 2023,
+        description: ``,
+        icon: `url("Assets/Images/Games/NightKnight/icon.png")`,
+        screenshots: [
             //...
         ]
     },
 ];
+
+// Functions
+function getVMin(){
+    return (window.innerWidth <= window.innerHeight ? window.innerWidth : window.innerHeight) / 100;
+}
+
 
 // Event functions
 function goToDiv(div){
     document.getElementById(div).scrollIntoView();
 }
 
-// Functions
-function createProjectButton(project){
+function scrollToRight(div){
+    document.getElementById(div).scrollLeft += getVMin() * 31;
+}
 
+function scrollToLeft(div){
+    document.getElementById(div).scrollLeft -= getVMin() * 31;
 }
 
 // Program
 projects.forEach(project => {
-    document.getElementById("projects_list").innerHTML += `
-        <div class="btn_project" id="btn_${project.name}">
-            <div class="btn_project_name">${project.name}</div>
-        </div>
-    `;
-
-    document.getElementById(`btn_${project.name}`).style.backgroundImage = project.screenshots[0];
+    if(!document.getElementById(`projects_year_list_${project.year}`)){
+        document.getElementById("projects_list").innerHTML += 
+        `
+            <div class="projects_year">
+                <div class="projects_year_list" id="projects_year_list_${project.year}">
+                    <!-- Filled with projects -->
+                </div>
+                <div class="projects_year_indicator">
+                    <p>${project.year}</p>
+                    <hr/>
+                </div>
+            </div>
+        `;
+    }
+    document.getElementById(`projects_year_list_${project.year}`).innerHTML += `
+            <div class="btn_project" id="btn_${project.name}">
+                <div class="btn_project_name">${project.name}</div>
+            </div>
+        `;
+        document.getElementById(`btn_${project.name}`).style.backgroundImage = project.icon;
 });
